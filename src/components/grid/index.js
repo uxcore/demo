e63e8069f6a5db162ec6b258d0e58e59c0f70b52
biangang.bg <biangang.bg@alibaba-inc.com>
2016-04-13 10:09:23 +0800

    up style

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -92,47 +92,47 @@ class Demo extends React.Component {
                         <h3>2栏</h3>
                         <div className="kuma-container-1180 fn-border-box">
                             <div className="kuma-row">
-                            <div className="col-xs-8"><div className="grid-item-1">220px</div></div>
-                            <div className="col-xs-16"><div className="grid-item-1">940px</div></div>
+                            <div className="col-xs-8"><div className="grid-item-1">380px</div></div>
+                            <div className="col-xs-16"><div className="grid-item-1">780px</div></div>
                             </div>
                             <div className="kuma-row">
-                            <div className="col-xs-9"><div className="grid-item-1">280px</div></div>
-                            <div className="col-xs-15"><div className="grid-item-1">880px</div></div>
+                            <div className="col-xs-9"><div className="grid-item-1">430px</div></div>
+                            <div className="col-xs-15"><div className="grid-item-1">730px</div></div>
                             </div>
                             <div className="kuma-row">
-                            <div className="col-xs-10"><div className="grid-item-1">340px</div></div>
-                            <div className="col-xs-14"><div className="grid-item-1">820px</div></div>
+                            <div className="col-xs-10"><div className="grid-item-1">480px</div></div>
+                            <div className="col-xs-14"><div className="grid-item-1">680px</div></div>
                             </div>
                             <div className="kuma-row">
                             <div className="col-xs-12"><div className="grid-item-1">580px</div></div>
                             <div className="col-xs-12"><div className="grid-item-1">580px</div></div>
                             </div>
                             <div className="kuma-row">
-                            <div className="col-xs-18"><div className="grid-item-1">820px</div></div>
-                            <div className="col-xs-6"><div className="grid-item-1">340px</div></div>
+                            <div className="col-xs-18"><div className="grid-item-1">880px</div></div>
+                            <div className="col-xs-6"><div className="grid-item-1">280px</div></div>
                             </div>
                         </div>
                         <h3>3栏</h3>
                         <div className="kuma-container-1180 fn-border-box">
                             <div className="kuma-row">
-                            <div className="col-xs-6"><div className="grid-item-1">220px</div></div>
-                            <div className="col-xs-12"><div className="grid-item-1">700px</div></div>
-                            <div className="col-xs-6"><div className="grid-item-1">220px</div></div>
+                            <div className="col-xs-6"><div className="grid-item-1">280px</div></div>
+                            <div className="col-xs-12"><div className="grid-item-1">580px</div></div>
+                            <div className="col-xs-6"><div className="grid-item-1">280px</div></div>
                             </div>
                             <div className="kuma-row">
-                            <div className="col-xs-7"><div className="grid-item-1">280px</div></div>
-                            <div className="col-xs-11"><div className="grid-item-1">640px</div></div>
-                            <div className="col-xs-6"><div className="grid-item-1">220px</div></div>
+                            <div className="col-xs-7"><div className="grid-item-1">330px</div></div>
+                            <div className="col-xs-11"><div className="grid-item-1">530px</div></div>
+                            <div className="col-xs-6"><div className="grid-item-1">280px</div></div>
                             </div>
                             <div className="kuma-row">
-                            <div className="col-xs-6"><div className="grid-item-1">220px</div></div>
-                            <div className="col-xs-11"><div className="grid-item-1">640px</div></div>
-                            <div className="col-xs-7"><div className="grid-item-1">280px</div></div>
+                            <div className="col-xs-6"><div className="grid-item-1">280px</div></div>
+                            <div className="col-xs-11"><div className="grid-item-1">530px</div></div>
+                            <div className="col-xs-7"><div className="grid-item-1">330px</div></div>
                             </div>
                             <div className="kuma-row">
-                            <div className="col-xs-6"><div className="grid-item-1">220px</div></div>
-                            <div className="col-xs-5"><div className="grid-item-1">280px</div></div>
-                            <div className="col-xs-13"><div className="grid-item-1">640px</div></div>
+                            <div className="col-xs-6"><div className="grid-item-1">280px</div></div>
+                            <div className="col-xs-5"><div className="grid-item-1">230px</div></div>
+                            <div className="col-xs-13"><div className="grid-item-1">630px</div></div>
                             </div>
                         </div>
                 </div>
